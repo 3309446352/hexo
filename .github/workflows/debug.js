46b7b05ea2c92de069090e42e6f136a719348839
顾行 <xuechengdugu@gmail.com>
2026-02-17 05:40:55 +0800

    打错了

---
+++ .github/workflows/debug.js
@@ -0,0 +1,5 @@
+console.log('=== Node.js 环境变量 ===');
+console.log('IMAGES_TOKEN:', process.env.IMAGES_TOKEN ? '存在' : '不存在');
+console.log('IMAGES_OWNER:', process.env.IMAGES_OWNER);
+console.log('IMAGES_REPO:', process.env.IMAGES_REPO);
+console.log('======================');
