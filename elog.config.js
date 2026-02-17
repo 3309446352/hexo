7d65c5879fec549076b01d5f86bce2df68eb12c4
顾行 <xuechengdugu@gmail.com>
2026-02-17 14:28:24 +0800

    打错了

--- elog.config.js
+++ elog.config.js
@@ -37,9 +37,9 @@ module.exports = {
     platform: 'github',  // 改为 github
     github: {
       token: process.env.IMAGES_TOKEN,
-      user: '3309446352',                  // GitHub 用户名
-      repo: 'Images',                  // 存放图片的仓库名
-      branch: 'main',                          // 分支名（默认 main）       // 图片存储路径
+      user: process.env.IMAGES_USER,                  // GitHub 用户名
+      repo: process.env.IMAGES_REPO,                  // 存放图片的仓库名
+      branch: process.env.IMAGES_BRANCH,                          // 分支名（默认 main）       // 图片存储路径
       host: 'cdn.jsdelivr.net',// GitHub Token
       prefixKey: 'img/'  // 图片访问前缀
     }
