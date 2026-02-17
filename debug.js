eb3d485db5fbb1cd62ec1d8b8141960cbd5ebff4
顾行 <xuechengdugu@gmail.com>
2026-02-17 13:51:36 +0800

    打错了

--- debug.js
+++
@@ -1,5 +0,0 @@
-console.log('=== Node.js 环境变量 ===');
-console.log('IMAGES_TOKEN:', process.env.IMAGES_TOKEN ? '存在' : '不存在');
-console.log('IMAGES_OWNER:', process.env.IMAGES_OWNER);
-console.log('IMAGES_REPO:', process.env.IMAGES_REPO);
-console.log('======================');
